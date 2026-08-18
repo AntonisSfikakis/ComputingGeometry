18f4ed9a3498b2660c1d0d50e4a8d67172a2d2dc
AntonisSfikakis <sdi2200178@di.uoa.gr>
2026-08-18 15:40:29 +0300

    Incremental Done

--- A/_1.py
+++ A/_1.py
@@ -1,18 +1,15 @@
 import numpy as np
 
-1# P is a set of pairs (aka tuples) , mayber ordered later
+# P is a set of pairs (aka tuples) , mayber ordered later
 def Incremental(P: set[tuple[int, int]]):
     # step 1 : sort by x
     P_sorted = tuple(sorted(P, key=lambda x: x[0]))
-    print(f"{P_sorted}")
 
     # step 2 : create L_upp and add p1 and p2
     L_upp : list[tuple[int,int]] = []
     L_upp.append(P_sorted[0])
     L_upp.append(P_sorted[1])
-    print(f"{L_upp}")
 
-    print(f"{len(P_sorted)}")
     # step 3 : Add points to L_upp , remove the second last when the turn is left
     for i in range(2, len(P)): 
         L_upp.append(P_sorted[i])
@@ -24,31 +21,50 @@ def Incremental(P: set[tuple[int, int]]):
             #                [ 1 x3 y3 ]
 
             # det = 1 * [x2 y2] - x1 * [1 y2] + y1 [1 x2]
-            #           [x3 y3]        [1 y3] +    [1 x3z]
-            matrix = np.array([[1,L_upp[-3][0],L_upp[-3][1]],
-                               [1,L_upp[-2][0],L_upp[-2][1]],
-                               [1,L_upp[-1][0],L_upp[-1][1]]
-                            ])
+            #           [x3 y3]        [1 y3] +    [1 x3]
 
-            det = np.linalg.det(matrix)
-            # the  middle of the three is the last of L_upp  (p_(i-1))
+            # det = x2y3 - x3y2 -x1y3 + x1y2 + y1x3 - y1x2
+            
+            x1 = L_upp[-3][0]
+            y1 = L_upp[-3][1]
+            x2 = L_upp[-2][0]
+            y2 = L_upp[-2][1]
+            x3 = L_upp[-1][0]
+            y3 = L_upp[-1][1]
+
+
+            det = x2*y3 - x3*y2 -x1*y3 + x1*y2 + y1*x3 - y1*x2
+
             if det >= 0:
                 L_upp.pop(-2)
             else:
                 break
 
-    print(f"RESULT : {L_upp}")
 
     # step 4 : create L_down add p_(i-1) and p_(i)
     L_down : list[tuple[int, int]] = []
-    L_down.append(P_sorted[-2])
     L_down.append(P_sorted[-1])
-    print(f"{L_down}")
+    L_down.append(P_sorted[-2])
 
-    print("LOOP STARTS")
+    # set 5 : add to L_down till only two points left or there is a right 
     for j in range(len(P_sorted) - 3, -1, -1):
-        print(f"{P_sorted[j]}")
+        L_down.append(P_sorted[j])
 
+        while (len(L_down) > 2):
+            x1,y1 = L_down[-3]
+            x2,y2 = L_down[-2]
+            x3,y3 = L_down[-1]
 
-P = set([(10,27), (5,6), (7,8), (0,2), (2,5)])
-Incremental(P)
+            det = x2*y3 - x3*y2 -x1*y3 + x1*y2 + y1*x3 - y1*x2
+
+            if det >= 0:
+                L_down.pop(-2)
+            else:
+                break
+
+    # step 6 : remove first and last element
+    L_down.pop(-1)
+    L_down.pop(0)
+
+    L = L_upp + L_down
+    return L
